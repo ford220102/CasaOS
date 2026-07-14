a5d28391d948ff8724a2047c2231acf09901b1e0
ford220102 <138443348+ford220102@users.noreply.github.com>
2026-07-14 11:32:10 +0200

    Update config.go
    Signed-off-by: ford220102 <138443348+ford220102@users.noreply.github.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,19 +1,42 @@
-/*
- * @Author: LinkLeong link@icewhale.com
- * @Date: 2021-09-30 18:18:14
- * @LastEditors: LinkLeong
- * @LastEditTime: 2022-08-31 17:04:02
- * @FilePath: /CasaOS/pkg/config/config.go
- * @Description:
- * @Website: https://www.casaos.io
- * Copyright (c) 2022 by icewhale, All Rights Reserved.
- */
 package config
 
 import (
+	"os"
 	"path/filepath"
 
-	"github.com/IceWhaleTech/CasaOS-Common/utils/constants"
+	"github.com/IceWhaleTech/CasaOS/model"
+	"github.com/IceWhaleTech/CasaOS/pkg/jwt"
+	"github.com/sirupsen/logrus"
 )
 
-var CasaOSConfigFilePath = filepath.Join(constants.DefaultConfigPath, "casaos.conf")
+// InitConfig inicjalizuje wszystkie konfiguracje
+func InitConfig() error {
+	// Inicjalizuj JWT secret
+	if err := jwt.InitJWTSecret(); err != nil {
+		logrus.Errorf("Failed to init JWT secret: %v", err)
+		return err
+	}
+
+	// Tutaj inne inicjalizacje konfiguracji...
+	
+	return nil
+}
+
+// GetJWTSecretPath zwraca ścieżkę do pliku z sekretem
+func GetJWTSecretPath() string {
+	// Możesz użyć zmiennej środowiskowej lub domyślnej ścieżki
+	if path := os.Getenv("JWT_SECRET_PATH"); path != "" {
+		return path
+	}
+	
+	// Dla Dockera: /var/lib/casaos/jwt_secret.key
+	// Dla Windows: C:\ProgramData\CasaOS\jwt_secret.key
+	// Dla Linux: /var/lib/casaos/jwt_secret.key
+	
+	baseDir := "/var/lib/casaos"
+	if os.Getenv("CASAOS_BASE_PATH") != "" {
+		baseDir = os.Getenv("CASAOS_BASE_PATH")
+	}
+	
+	return filepath.Join(baseDir, "jwt_secret.key")
+}
